32d1736661963dba41dcc6e9c5c02d827549f259
Leandro Ibraim <lreis@scopus.com.br>
2020-01-22 16:55:06 -0300

    Adiciona o pacote de tabuleiro e a classe de testes

---
+++ application/teste_mesa/TestesDiversos.java
@@ -0,0 +1,17 @@
+package teste_mesa;
+
+import tabuleiro.Posicao;
+
+public class TestesDiversos {
+	public static void main(String[] args) {
+	
+		Posicao p = new Posicao(10, 10);
+		System.out.println(p);
+		
+	}
+	
+	
+	
+	
+
+}
