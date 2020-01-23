4ed142725a4f886b9a7799d70e4bf0e0c692827e
Leandro Ibraim <lreis@scopus.com.br>
2020-01-23 10:03:24 -0300

    programacao defensiva

--- application/boardgame/Board.java
+++ application/boardgame/Board.java
@@ -6,39 +6,63 @@ public class Board {
 	private Piece[][] pieces;
 
 	public Board(int rows, int columns) {
-		this.setRows(rows);
-		this.setColumns(columns);
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Erro ao criar o tabuleiro é necessario pelomenos uma linha e uma coluna! ");
+		}
+		this.rows = rows;
+		this.columns = columns;
 		pieces = new Piece[rows][columns];
 	}
-	
-	
+
 	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Posicao nao existe no tabuleiro! ");
+
+		}
 		return pieces[row][column];
 	}
-	
+
 	public Piece piece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posicao nao existe! ");
+
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
-	
+
 	public void placePiece(Piece piece, Position position) {
+		if (thereIsApiece(position)) {
+			throw new BoardException("ja existe a peca na posicao! " + position);
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
-	
+
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+
+	}
+
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+
+	}
+
+	public boolean thereIsApiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posicao ja existe! ");
+
+		}
+		return piece(position) != null;
+
+	}
+
 	public int getRows() {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
-	}
-
-	public void setColumns(int columns) {
-		this.columns = columns;
 	}
 
 }
--- application/chess/ChessMatch.java
+++ application/chess/ChessMatch.java
@@ -27,8 +27,8 @@ public class ChessMatch {
 	}
 	private void initialSetup() {
 		board.placePiece(new Rook(board,  Color.WHITE), new Position(1,1));
-		board.placePiece(new King(board,  Color.BLACK), new Position(0,5));
-		board.placePiece(new King(board,  Color.BLACK), new Position(1,7));
+		board.placePiece(new King(board,  Color.BLACK), new Position(1,4));
+		board.placePiece(new King(board,  Color.BLACK), new Position(2,4));
 	}
 	
 
